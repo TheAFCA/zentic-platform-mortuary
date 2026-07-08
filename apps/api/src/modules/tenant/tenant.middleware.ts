9a1bc153ee3ab6676371474a02b9bfb49153e92b
David Eduardo Delgado Herrera <david.delgado@banistmo.com>
2026-07-07 20:04:24 -0500

    fix(api): suppress prisma lint false positives

--- apps/api/src/modules/tenant/tenant.middleware.ts
+++ apps/api/src/modules/tenant/tenant.middleware.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unsafe-call */
 import { Injectable, NestMiddleware, NotFoundException } from '@nestjs/common';
 import { Request, Response, NextFunction } from 'express';
 import { PrismaService } from '../../prisma/prisma.service';
