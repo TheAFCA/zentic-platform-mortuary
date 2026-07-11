03c6449c564277aa1b08be8ee7557809917cd6dc
Edgarciapame <268042255+Edgarciapame@users.noreply.github.com>
2026-07-11 12:26:04 -0500

    feat(streaming): add NotificationsModule to streaming module imports

--- apps/api/src/modules/streaming/streaming.module.ts
+++ apps/api/src/modules/streaming/streaming.module.ts
@@ -2,8 +2,10 @@ import { Module } from '@nestjs/common';
 import { StreamingController } from './streaming.controller';
 import { StreamingService } from './streaming.service';
 import { StreamingRepository } from './streaming.repository';
+import { NotificationsModule } from '../notifications/notifications.module';
 
 @Module({
+  imports: [NotificationsModule],
   controllers: [StreamingController],
   providers: [StreamingService, StreamingRepository],
   exports: [StreamingService],
